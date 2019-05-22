eb0d645bcfcc86f754ffa0795b8076a77ac8ae99
hungnguyen9696 <hungnvh96@gmail.com>
2019-05-22 20:10:24 +0300

    update

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ const port = process.env.PORT || 3000;
 console.log(`Listening at port ${port}`);
 
 app.listen(port);
+
+const axios = require("axios");
+const url =
+  "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=godfather&api-key=S8qOkuUQwRGgue2rzIQ9ZeTPHC3dABNh";
+const getData = async url => {
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+getData(url);
